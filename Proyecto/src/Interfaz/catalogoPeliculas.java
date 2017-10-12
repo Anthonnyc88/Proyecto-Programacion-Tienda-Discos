d874b3412bd96a861030963ae9e31ee78ac0170d
punts hola mundo <anthonnyc10@gmail.com>
2017-10-12 11:39:51 -0600

    hecho Catalogo peliculas

--- Proyecto/src/Interfaz/catalogoPeliculas.java
+++ Proyecto/src/Interfaz/catalogoPeliculas.java
@@ -5,9 +5,16 @@
  */
 package Interfaz;
 
+import Datos.ArchivoPeliculas;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -19,11 +26,49 @@ public class catalogoPeliculas extends javax.swing.JFrame {
     /**
      * Creates new form catalogoPeliculas
      */
+//    public void agregarMusica() {
+//        File f;
+//        FileWriter w;
+//        BufferedWriter bw;
+//        PrintWriter wi;
+//        String texto = "";
+//        File archivo;
+//        try {
+//            archivo = new File("C:\\Users\\Anthonny\\Desktop\\Proyecto\\ProyectoTiendaDiscos\\Proyecto\\Musica.txt"); //Aqui ponemos el nombre del archivo que vamos a crear y la ruta
+//
+//            if (archivo.createNewFile()) {//crea el archivo
+//                System.out.println("Se ha creado un archivo");
+//            }
+//            w = new FileWriter(archivo, true);
+//            bw = new BufferedWriter(w);
+//            wi = new PrintWriter(bw);
+//            String nombre;
+//            String autor;
+//            int nota, nota2, nota3;
+//            int cantidad;
+//            nombre = textNombredisco.getText();
+//            autor = textAutor.getText();
+//            cantidad = Integer.parseInt(textCantidad.getText());
+//
+//            bw.write(nombre + " ");
+//            bw.write(autor + " ");
+//            bw.write(String.valueOf(new Integer(cantidad) + " "));
+//
+//            bw.newLine();
+//            wi.close();
+//            bw.close();
+//
+//        } catch (IOException e) {
+//            System.err.println("No se ha creado el archivo" + e);
+//
+//        }
+//
+//    }
     public catalogoPeliculas() {
         initComponents();
         setLocationRelativeTo(null);
-         setResizable(false);
-         setTitle("Tienda de discos");
+        setResizable(false);
+        setTitle("Tienda de discos");
         setIconImage(new ImageIcon(getClass().getResource("/Imagenes/Disco.jpg")).getImage());
 
         ((JPanel) getContentPane()).setOpaque(false);
@@ -47,12 +92,14 @@ public class catalogoPeliculas extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
+        comboCategorias = new javax.swing.JComboBox();
         textNombredisco = new javax.swing.JTextField();
         textAutor = new javax.swing.JTextField();
         textCantidad = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         bntAgregar = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        textPrecio = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -72,30 +119,51 @@ public class catalogoPeliculas extends javax.swing.JFrame {
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Cantidad");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        comboCategorias.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Terror", "Suspenso", "Comedia", "Romance", "Accion" }));
 
         jLabel6.setBackground(new java.awt.Color(255, 255, 255));
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("Categoria");
 
         bntAgregar.setText("Agregar");
+        bntAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bntAgregarActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Precio");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(156, 156, 156)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addGap(22, 22, 22)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.Alignment.LEADING, 0, 59, Short.MAX_VALUE)
-                    .addComponent(textCantidad, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(textAutor, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(textNombredisco))
-                .addGap(409, 409, 409))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(156, 156, 156)
+                        .addComponent(jLabel4)
+                        .addGap(26, 26, 26))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(textCantidad, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
+                            .addComponent(textAutor, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(textNombredisco))
+                        .addGap(409, 409, 409))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(comboCategorias, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(textPrecio))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(layout.createSequentialGroup()
                 .addGap(267, 267, 267)
                 .addComponent(jLabel1)
@@ -110,7 +178,7 @@ public class catalogoPeliculas extends javax.swing.JFrame {
                         .addGap(494, 494, 494))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(bntAgregar)
-                        .addGap(296, 296, 296))))
+                        .addGap(287, 287, 287))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -132,15 +200,28 @@ public class catalogoPeliculas extends javax.swing.JFrame {
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(62, 62, 62)
+                    .addComponent(comboCategorias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(textPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(47, 47, 47)
                 .addComponent(bntAgregar)
-                .addGap(68, 68, 68))
+                .addGap(31, 31, 31))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bntAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bntAgregarActionPerformed
+        // TODO add your handling code here:
+        ArchivoPeliculas peliculas = new ArchivoPeliculas();
+        String datosPeliculas = textNombredisco.getText() + "," + textAutor.getText() + "," + textCantidad.getText() + "," + comboCategorias.getSelectedItem() + "," + textPrecio.getText();
+        peliculas.GuardarArchivoPeliculas("catalogoPeliculas.txt", datosPeliculas);
+
+        JOptionPane.showMessageDialog(null, "Agregado al sistema");
+    }//GEN-LAST:event_bntAgregarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -179,14 +260,16 @@ public class catalogoPeliculas extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bntAgregar;
-    private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JComboBox comboCategorias;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JTextField textAutor;
     private javax.swing.JTextField textCantidad;
     private javax.swing.JTextField textNombredisco;
+    private javax.swing.JTextField textPrecio;
     // End of variables declaration//GEN-END:variables
 }
