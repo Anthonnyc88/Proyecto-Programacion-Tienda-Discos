723d614f37bb0535f263514dffbfd1484139fb6a
Admie21 <Admie21@MP161834>
2017-10-12 09:37:11 -0600

    Primer Commit Tienda Discos

---
+++ Proyecto/src/proyecto/Catalogo.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Anthonny
+ */
+public class Catalogo {
+
+    public String nombre;
+    public String autor;
+    public String categoria;
+    public int precio;
+    public int cantidadDisponible;
+
+    public Catalogo() {
+    }
+
+    public Catalogo(String nombre, String autor, String categoria, int precio, int cantidadDisponible) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+}
---
+++ Proyecto/src/proyecto/Peliculas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Anthonny
+ */
+public class Peliculas extends Catalogo{
+
+    public Peliculas() {
+    }
+
+    public Peliculas(String nombre, String autor, String categoria, int precio, int cantidadDisponible) {
+        super(nombre, autor, categoria, precio, cantidadDisponible);
+    }
+    
+    
+    
+}
