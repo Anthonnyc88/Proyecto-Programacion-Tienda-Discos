0671b008be21ea645e2fb30c08df9f35ff3b5c30
Admie21 <Admie21@MP161834>
2017-10-26 15:32:06 -0600

    ya agrego las ordenes al archivo de ordendes

--- Proyecto/src/Interfaz/Reporte1.java
+++ Proyecto/src/Interfaz/Reporte1.java
@@ -5,8 +5,8 @@
  */
 package Interfaz;
 
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.DefaultCategoryDataset;
+//import org.jfree.chart.JFreeChart;
+//import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
@@ -14,8 +14,8 @@ import org.jfree.data.category.DefaultCategoryDataset;
  */
 public class Reporte1 extends javax.swing.JFrame {
 
-    JFreeChart Grafica;
-    DefaultCategoryDataset Datos = new DefaultCategoryDataset();
+    //JFreeChart Grafica;
+    //DefaultCategoryDataset Datos = new DefaultCategoryDataset();
 
     public Reporte1() {
         initComponents();
@@ -23,13 +23,13 @@ public class Reporte1 extends javax.swing.JFrame {
         setResizable(false);
         setTitle("Tienda de discos");
 
-        Datos.addValue(1, "Negocio 1", "Lunes");
-        Datos.addValue(2, "Negocio 1", "Martes");
-        Datos.addValue(3, "Negocio 1", "Miércoles");
-        Datos.addValue(4, "Negocio 1", "Jueves");
-        Datos.addValue(5, "Negocio 1", "Viernes");
-        Datos.addValue(6, "Negocio 1", "Sábado");
-        Datos.addValue(7, "Negocio 1", "Domingo");
+//        Datos.addValue(1, "Negocio 1", "Lunes");
+//        Datos.addValue(2, "Negocio 1", "Martes");
+//        Datos.addValue(3, "Negocio 1", "Miércoles");
+//        Datos.addValue(4, "Negocio 1", "Jueves");
+//        Datos.addValue(5, "Negocio 1", "Viernes");
+//        Datos.addValue(6, "Negocio 1", "Sábado");
+//        Datos.addValue(7, "Negocio 1", "Domingo");
 
     }
 
--- Proyecto/src/Interfaz/buscarPeliculas.java
+++ Proyecto/src/Interfaz/buscarPeliculas.java
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import Datos.ArchivoOrdenes;
 
 /**
  *
@@ -222,6 +223,7 @@ public class buscarPeliculas extends javax.swing.JFrame {
             
             modelo.addElement(archivoPeliculas.verInformacionPelicula(textNombre.getText()));
             btnComprar.setVisible(true);
+            
 
 
         }else{
@@ -233,12 +235,51 @@ public class buscarPeliculas extends javax.swing.JFrame {
         // TODO add your handling code here:
         
                     
-        //String[] detalleInformacionPelicula = resultadoPeliculas.getSelectedValue().split(" / ");
-              
-        //int cantidaDeseada = Integer.parseInt(JOptionPane.showInputDialog("Introduzca la Cantidad deseada : "));
+         String detallePelicula = resultadoPeliculas.getSelectedValue().toString();
+         String [] detalleInformacionPelicula = detallePelicula.split(" / ");
+        
+         ArchivoOrdenes archivoPeliculasOrdenes = new ArchivoOrdenes();
+         
+        ArchivoBuscarPelicula archivoPeliculas = new ArchivoBuscarPelicula();
+         
+        int cantidaDeseada = Integer.parseInt(JOptionPane.showInputDialog("Introduzca la Cantidad deseada : "));
 
-        //int cantidadOriginal = Integer.parseInt(archivoPeliculas.verificarCantidadDisponiblePelicula(detalleInformacionPelicula[0], detalleInformacionPelicula[1]));
+        int cantidadOriginal = Integer.parseInt(archivoPeliculas.verificarCantidadDisponiblePelicula(detalleInformacionPelicula[0], detalleInformacionPelicula[1]));
 
+        if (cantidaDeseada > cantidadOriginal) {
+
+            int opcionPreOrden = Integer.parseInt(JOptionPane.showInputDialog("Cantidad no Disponible \nDesea realizar una Pre Orden \n1: SI \n2: NO  "));
+
+            if (opcionPreOrden == 1) {
+
+                //esto es lo que se va a ir al archivo de preordenes
+                String informacionPreOrden = detalleInformacionPelicula[0] + ";Pelicula;" + cantidaDeseada;
+
+                JOptionPane.showMessageDialog(null, "Pre Orden Realizada");
+
+                System.out.println(informacionPreOrden);
+            } else {
+                System.out.println("Pre Orden negada");
+            }
+
+        } else if (cantidaDeseada <= cantidadOriginal) {
+
+            String nombreCliente = JOptionPane.showInputDialog("Introduzca su Nombre : ");
+            String cedulaCliente = JOptionPane.showInputDialog("Introduzca su Numero de Cedula: ");
+            String correoCliente = JOptionPane.showInputDialog("Introduzca su Correo Electronico: ");
+            String nombrePelicula = detalleInformacionPelicula[0];
+            String cantidadOrdenada = String.valueOf(cantidaDeseada);
+
+            //esto es lo que vamos a escribir en el archivo de compras es el detalle total de la Orden
+            String detalleTotalOrdenPelicula = nombreCliente + ";" + cedulaCliente + ";" + correoCliente + ";" + nombrePelicula + ";" + cantidadOrdenada;
+
+            System.out.println(detalleTotalOrdenPelicula);
+            
+            archivoPeliculasOrdenes.registrarOrden("peliculasOrdenes.txt",detalleTotalOrdenPelicula);
+            
+            JOptionPane.showMessageDialog(null, "Compra Realizada");
+
+        }
         
     }//GEN-LAST:event_btnComprarActionPerformed
 
