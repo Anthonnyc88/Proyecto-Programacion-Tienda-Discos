5c50f24a7ede449d12c38b29b38d67fd968cbdd2
Admie21 <Admie21@MP161834>
2017-10-26 10:56:19 -0600

    Agrego metodos para las Ordenes y Pre ordenes

--- Proyecto/src/Datos/ArchivoOrdenes.java
+++ Proyecto/src/Datos/ArchivoOrdenes.java
@@ -5,10 +5,67 @@
  */
 package Datos;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author Admie21
  */
 public class ArchivoOrdenes {
+    
+    
+    /**
+     * Metodo que registra las Ordenes ya realizadas
+     * @param nombreArchivo el archivo de las Ordenes
+     * @param detalleOrden el detalle de la Orden
+     */
+     public void registrarOrden(String nombreArchivo, String detalleOrden) {
+        File f;
+        /**
+         * se crea un objeto de tipo archivo
+         *
+         * @param
+         */
+        f = new File(nombreArchivo);
+        /**
+         * se asigna el nombre al archivo como parametro obligatorio
+         *
+         * @param recibe como parametro el nombre archivo
+         *
+         */
+
+        try {
+            FileWriter fw = new FileWriter(f, true);//objeto que permite escribir en el archivo
+            BufferedWriter bw = new BufferedWriter(fw);// espacio de memoria para el archivo
+            PrintWriter pr = new PrintWriter(bw);//objeto que permite imprimir lo que hay en el archivo
+
+            f = new File(nombreArchivo);
+            if (f.exists()) {
+
+                fw = new FileWriter(f, true);//objeto que permite escribir en el archivo
+                bw = new BufferedWriter(fw);// espacio de memoria para el archivo
+
+                bw.newLine();
+                bw.write(detalleOrden);
+            } else {
+
+                fw = new FileWriter(f, true);//objeto que permite escribir en el archivo
+                bw = new BufferedWriter(fw);// espacio de memoria para el archivo
+
+                bw.write(detalleOrden);
+
+            }
+
+            bw.close();
+            fw.close();
+
+        } catch (IOException e) {
+        }
+
+    }
     
 }
--- Proyecto/src/Datos/ArchivoPreOrdenes.java
+++ Proyecto/src/Datos/ArchivoPreOrdenes.java
@@ -5,10 +5,66 @@
  */
 package Datos;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author Admie21
  */
 public class ArchivoPreOrdenes {
+    
+    /**
+     * Metodo que registra las Ordenes ya realizadas
+     * @param nombreArchivo el archivo de las Pre Ordenes
+     * @param detallePreOrden el detalle de la Pre-Orden
+     */
+     public void registrarPreOrden(String nombreArchivo, String detallePreOrden) {
+        File f;
+        /**
+         * se crea un objeto de tipo archivo
+         *
+         * @param
+         */
+        f = new File(nombreArchivo);
+        /**
+         * se asigna el nombre al archivo como parametro obligatorio
+         *
+         * @param recibe como parametro el nombre archivo
+         *
+         */
+
+        try {
+            FileWriter fw = new FileWriter(f, true);//objeto que permite escribir en el archivo
+            BufferedWriter bw = new BufferedWriter(fw);// espacio de memoria para el archivo
+            PrintWriter pr = new PrintWriter(bw);//objeto que permite imprimir lo que hay en el archivo
+
+            f = new File(nombreArchivo);
+            if (f.exists()) {
+
+                fw = new FileWriter(f, true);//objeto que permite escribir en el archivo
+                bw = new BufferedWriter(fw);// espacio de memoria para el archivo
+
+                bw.newLine();
+                bw.write(detallePreOrden);
+            } else {
+
+                fw = new FileWriter(f, true);//objeto que permite escribir en el archivo
+                bw = new BufferedWriter(fw);// espacio de memoria para el archivo
+
+                bw.write(detallePreOrden);
+
+            }
+
+            bw.close();
+            fw.close();
+
+        } catch (IOException e) {
+        }
+
+    }
     
 }
